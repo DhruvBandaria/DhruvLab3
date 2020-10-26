04a71bedc032fd1e9fcbebdc8f95f7d0fc63e8d0
Dhruv <dhruvbandaria@hotmail.com>
2020-10-26 16:05:25 -0400

    Added Bottom Navigation

--- app/src/main/java/dhruv/bandaria/s301102928/DhruvActivity.java
+++ app/src/main/java/dhruv/bandaria/s301102928/DhruvActivity.java
@@ -1,8 +1,13 @@
 package dhruv.bandaria.s301102928;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DhruvActivity extends AppCompatActivity {
 
@@ -10,5 +15,8 @@ public class DhruvActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dhruv);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
