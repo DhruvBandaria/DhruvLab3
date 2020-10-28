2428b8844465085239d7d7f1b299e150e0c5708b
Dhruv <dhruvbandaria@hotmail.com>
2020-10-27 20:51:03 -0400

    Added Framed Animation

--- app/src/main/java/dhruv/bandaria/s301102928/BandariaFragment.java
+++ app/src/main/java/dhruv/bandaria/s301102928/BandariaFragment.java
@@ -1,64 +1,90 @@
 package dhruv.bandaria.s301102928;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BandariaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BandariaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public BandariaFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BandariaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BandariaFragment newInstance(String param1, String param2) {
-        BandariaFragment fragment = new BandariaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    Button button,startAnimation,stopAnimation;
+    ImageView iv;
+    AnimationDrawable animationDrawable = null;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bandaria, container, false);
+        View v=inflater.inflate(R.layout.fragment_bandaria, container, false);
+        button=v.findViewById(R.id.open_call);
+        startAnimation=v.findViewById(R.id.startFrameAnimation);
+        stopAnimation=v.findViewById(R.id.stopFrameAnimation);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = "+34666777888";
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+                startActivity(intent);
+            }
+        });
+
+        startAnimation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
+
+        stopAnimation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopAnimation();
+            }
+        });
+        iv=v.findViewById(R.id.framedAnimation);
+        return v;
+    }
+
+    private void startAnimation(){
+        BitmapDrawable frame1=(BitmapDrawable) getResources().getDrawable(R.drawable.frame1);
+        BitmapDrawable frame2=(BitmapDrawable) getResources().getDrawable(R.drawable.frame2);
+        BitmapDrawable frame3=(BitmapDrawable) getResources().getDrawable(R.drawable.frame3);
+        BitmapDrawable frame4=(BitmapDrawable) getResources().getDrawable(R.drawable.frame4);
+        BitmapDrawable frame5=(BitmapDrawable) getResources().getDrawable(R.drawable.frame5);
+
+        int reasonableDuration = 250;
+
+        animationDrawable=new AnimationDrawable();
+        animationDrawable.setOneShot(false);
+        animationDrawable.addFrame(frame1,reasonableDuration);
+        animationDrawable.addFrame(frame2,reasonableDuration);
+        animationDrawable.addFrame(frame3,reasonableDuration);
+        animationDrawable.addFrame(frame4,reasonableDuration);
+        animationDrawable.addFrame(frame5,reasonableDuration);
+
+        iv.setBackground(animationDrawable);
+        animationDrawable.setVisible(true,true);
+        animationDrawable.start();
+        iv.setVisibility(View.VISIBLE);
+
+    }
+    private void stopAnimation(){
+        animationDrawable.stop();
+        animationDrawable.setVisible(false,false);
+        iv.setVisibility(View.INVISIBLE);
     }
 }
